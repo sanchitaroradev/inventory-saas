08e536ee4bfd536a6c4a9d8b5d2a9c3c7861ab55
sanchitaroradev <sanchitarora.dev@gmail.com>
2026-02-27 23:15:59 +0530

    Setup MongoDB Atlas connection and improved server startup flow

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,25 @@
+import dotenv from "dotenv"
+dotenv.config()
 import  express  from "express";
+import { connectDB } from "./config/db";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-const PORT = process.env.PORT || 5000
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
 app.get("/",(req,res)=>{
     res.json({
         message: "Server is running successfully"
-    })
+    });
 })
 
-app.listen(PORT,()=>{
-    console.log(`Server running on http://localhost:${PORT}`)
-})
+const startServer= async ()=>{
+    await connectDB();
+
+    app.listen(PORT,()=>{
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+};
+
+startServer();
