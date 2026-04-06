9bb50b3e129adc423427203e6063cf5b6afa1761
sanchitaroradev <sanchitarora.dev@gmail.com>
2026-04-06 22:18:38 +0530

    Initialized frontend with Vite, Tailwind setup, basic project structure

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,10 @@
+
+const Login = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-3xl font-bold">Login Page</h1>
+    </div>
+  );
+};
+
+export default Login
